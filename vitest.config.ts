ff11d894047c65999722f853e495130342a61278
Magno Korzekwa <magno20101@gmail.com>
2026-01-31 16:05:30 -0300

    Fixes on unit testing and packaging

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,9 +19,6 @@ export default mergeConfig(
         reporter: ['text', 'json', 'html'],
         thresholds: {
           lines: 80,
-          functions: 80,
-          branches: 80,
-          statements: 80
         },
         exclude: [
           'src/types/**',
